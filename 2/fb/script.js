96074263b73899dfc35ed78adf2a0d06a4f3b00e
Rahman <rahman@pop-os.localdomain>
2023-10-02 13:43:55 +0530

    DarkMode added

--- 2/fb/script.js
+++ 2/fb/script.js
@@ -1,4 +1,53 @@
-let noti_menu = document.querySelector('.notifications-button')
-noti_menu.addEventListener('click',function(){
-    noti_menu.classList.toggle('active');   
+let profile_menu = document.querySelector('.profile-menu')
+let profile_pic = document.querySelector('.profile-pic')
+let noti_btn= document.querySelector('.dropdown-notifications')
+let noti_menu = document.querySelector('.menu-notifications ')
+let mes_btn = document.querySelector('.dropdown-messages')
+let mes_menu = document.querySelector('.menu-messages')
+let drk_btn = document.querySelector('.dark-btn')
+
+let dropdown = []
+
+
+mes_btn.addEventListener('click',()=>{
+    if (dropdown.length > 0){
+        for (let d of dropdown){
+            d.classList.remove('open')
+            dropdown.pop()
+        }
+    }
+    else{
+        mes_menu.classList.add('open')
+        dropdown.push(mes_menu)
+    }
 })
+noti_btn.addEventListener('click',function(){
+    if (dropdown.length > 0){
+        for (let d of dropdown){
+            d.classList.remove('open')
+            dropdown.pop()
+
+        }
+    }
+    else{
+        noti_menu.classList.add('open');  
+        dropdown.push(noti_menu) 
+    }
+})
+profile_pic.addEventListener('click',()=>{
+    if (dropdown.length > 0){
+        for (let d of dropdown){
+            d.classList.remove('open')
+            dropdown.pop()
+
+        }
+    }
+    else{
+        profile_menu.classList.add('open')
+        dropdown.push(profile_menu)
+    }
+})
+
+function dark_theme(){
+    document.body.classList.toggle('dark-theme')
+}
